917831a36f238c1279b069d948869f5fe7f71a7c
William Hum <sam.andrew.hum@gmail.com>
2022-12-13 17:30:26 -0500

    Update code

--- Big Data Project/WillExploration.R
+++ Big Data Project/WillExploration.R
@@ -14,20 +14,22 @@ dat <- filter(dat,OCCDVGYW<=3)#the numbers of years in current occupation.
 dat <- filter(dat, AGEDVGRP<=4)#age in four groups, youngest to oldest
 dat <- filter(dat,ICJ_05D<=2)#different work task? potential instrument
 dat <- filter(dat,ICJ_05I<=2)#loss of income 1 = yes 2 = no
-
+dat <- filter(dat,ICJ_05C<=2)#more stressed at work due to COVID 19? 1 = yes 2 = no
+dat <- filter(dat,ICJ_05B<=2)#more conflict 1 = yes 2 = no
 
 #Modicification of variables (mostly changing things to binary/bernoulli)
 dat <- mutate(dat,mental_health = ifelse(GEN_20 <= 3,0,1)) #changed to Bernoulli, 0 means the same or better
 dat <- mutate(dat, workload = ifelse(ICJ_05E == 2,0,1)) # changed workload to Bernoulli
-dat <- mutate(dat, unpaidleave = ifelse(ICJ_05L == 1,1,0)) #unpaid leave is 1, didnt take is 0
+dat <- mutate(dat, unpaid_leave = ifelse(ICJ_05L == 1,1,0)) #unpaid leave is 1, didnt take is 0
 dat <- mutate(dat, loneliness = ifelse(IM_10D == 1,1,0)) #1 is experienced loneliness due to covid, 0 is didnt
 dat <- mutate(dat, male = ifelse(GDRDVGRP == 1,1,0)) #1 if male, 0 if female
 dat <- mutate(dat, closedeath = ifelse(IM_10C == 2,0,1)) #0 if no, 1 if yes ; no significant effect
 dat <- mutate(dat, NumberYearFac = as.factor(OCCDVGYW)) # Less than 10 years / 10 to 19 years / 20 years or more
 dat <- mutate(dat, AgeFac = as.factor(AGEDVGRP)) # divide age into different segmentation
 dat <- mutate(dat, diffTask = ifelse(ICJ_05D == 1,0,1)) #1 is different task
-dat <- mutate(dat, incomeloss = ifelse(ICJ_05I == 2,0,1)) # 1 is loss of income
-
+dat <- mutate(dat, incomeloss = ifelse(ICJ_05I == 2,0,1)) # 1 is loss of income due to COVID-19 pandemic
+dat <- mutate(dat, more_stress = ifelse(ICJ_05C == 2,0,1)) #1 is more stressed from COVID-19 pandemic
+dat <- mutate(dat, more_conflict = ifelse(ICJ_05B == 2,0,1))# 1 is more conflict between employees and management due to covid
 
 #dat["LMAGNOC"][dat["LMAGNOC"] == "2"] <- "Nurse" #LMAGNOC is occupation within healthcare industry
 #dat["LMAGNOC"][dat["LMAGNOC"] == "3"] <- "Supportworker" #this is personal support worker or care aides
@@ -48,8 +50,8 @@ summary(ln0) #so far increased workload increases probability of having worse me
 
 #Finding our model
 #adding other variables
-full_model <- lm(mental_health ~ workload + unpaidleave + loneliness + male + AgeFac + NumberYearFac + diffTask, data=dat) #unpaid leave probably related to workload
-summary(full_model)
+full_model <- lm(mental_health ~ workload + unpaid_leave + loneliness + male + AgeFac + NumberYearFac + diffTask, data=dat) #unpaid leave probably related to workload
+#summary(full_model)
 #Interactions: male is not an interaction, occupation may not work
 
 # instrument: Age group of respondents in increments of 10
@@ -58,17 +60,20 @@ summary(full_model)
 #             different work task
 
 #instrument testing
-ln3 <- lm(workload ~ diffTask + unpaidleave + loneliness + male + AgeFac + NumberYearFac, data=dat) #testing an instrument (diffTask)
-summary(ln3)
+ln3 <- lm(workload ~ diffTask + unpaid_leave + loneliness + male + AgeFac + NumberYearFac, data=dat) #testing an instrument (diffTask)
+#summary(ln3)
 
 #"preliminary regressions"
 ln1 <- lm(mental_health ~ workload, data=dat)
 summary(ln1)
-ln2 <- lm(mental_health ~ workload + unpaidleave, data=dat)
+ln2 <- lm(mental_health ~ workload + unpaid_leave, data=dat)
 summary(ln2)
-ln3 <- lm(mental_health ~ workload + unpaidleave + loneliness, data=dat)
-ln4 <- lm(mental_health ~ workload + unpaidleave + loneliness + male, data=dat)
-ln5 <- lm(mental_health ~ workload + unpaidleave + loneliness + male + AgeFac, data=dat)
-ln6 <- lm(mental_health ~ workload + unpaidleave + loneliness + male + AgeFac + NumberYearFac, data=dat)
-ln7 <- lm(mental_health ~ workload + unpaidleave + loneliness + male + AgeFac + NumberYearFac + incomeloss, data=dat)
-summary(ln7)
+ln3 <- lm(mental_health ~ workload + unpaid_leave + loneliness, data=dat)
+ln4 <- lm(mental_health ~ workload + unpaid_leave + loneliness + male, data=dat)
+ln5 <- lm(mental_health ~ workload + unpaid_leave + loneliness + male + AgeFac, data=dat)
+ln6 <- lm(mental_health ~ workload + unpaid_leave + loneliness + male + AgeFac + NumberYearFac, data=dat)
+ln7 <- lm(mental_health ~ workload + unpaid_leave + loneliness + male + AgeFac + NumberYearFac + incomeloss, data=dat)
+ln8 <- lm(mental_health ~ workload + unpaid_leave + loneliness + male + AgeFac + NumberYearFac + incomeloss + more_stress, data=dat)
+ln9 <- lm(mental_health ~ workload + unpaid_leave + loneliness + male + AgeFac + NumberYearFac + incomeloss + more_stress + more_conflict, data=dat)
+
+#summary(ln9)

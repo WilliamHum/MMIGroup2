a5cd4f7533197bf0c3000ca9edbe4d5906659f0a
William Hum <sam.andrew.hum@gmail.com>
2022-12-17 00:04:18 -0500

    a

--- Big Data Project/WillExploration.R
+++ Big Data Project/WillExploration.R
@@ -1,19 +1,19 @@
-# detach("package:stargazer",unload=T)
+detach("package:stargazer",unload=T)
 # # Delete it
-# remove.packages("stargazer")
+remove.packages("stargazer")
 # # Download the source
-# download.file("https://cran.r-project.org/src/contrib/stargazer_5.2.3.tar.gz", destfile = "stargazer_5.2.3.tar.gz")
+download.file("https://cran.r-project.org/src/contrib/stargazer_5.2.3.tar.gz", destfile = "stargazer_5.2.3.tar.gz")
 # # Unpack
-# untar("stargazer_5.2.3.tar.gz")
+untar("stargazer_5.2.3.tar.gz")
 # # Read the sourcefile with .inside.bracket fun
-# stargazer_src <- readLines("stargazer/R/stargazer-internal.R")
+stargazer_src <- readLines("stargazer/R/stargazer-internal.R")
 # # Move the length check 5 lines up so it precedes is.na(.)
-# stargazer_src[1990] <- stargazer_src[1995]
-# stargazer_src[1995] <- ""
+stargazer_src[1990] <- stargazer_src[1995]
+stargazer_src[1995] <- ""
 # # Save back
-# writeLines(stargazer_src, con="stargazer/R/stargazer-internal.R")
+writeLines(stargazer_src, con="stargazer/R/stargazer-internal.R")
 # # Compile and install the patched package
-# install.packages("stargazer", repos = NULL, type="source")
+install.packages("stargazer", repos = NULL, type="source")
 
 library(tidyverse)
 library(ggplot2)
